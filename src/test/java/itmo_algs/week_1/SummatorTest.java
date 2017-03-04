8c6ccfaf176087d644f2f6da7319f920790284dc
Maksim Kiryanov <mxvkiryanov@gmail.com>
2017-03-04 22:22:35 +0300

    Week 1
    * Create second exercise solution (ExpressionCalculator)
    * Write tests for second exercise solution
    * Refactor tests: take out common methods to parent class

--- src/test/java/itmo_algs/week_1/SummatorTest.java
+++ src/test/java/itmo_algs/week_1/SummatorTest.java
@@ -1,93 +1,44 @@
 package itmo_algs.week_1;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExternalResource;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static util.FileUtils.readFileToString;
-import static util.FileUtils.writeStringToFile;
 
 /**
  * @author maksim-kiryanov
  */
-public class SummatorTest {
-
-    private static final Path INPUT_FILE_PATH = Paths.get("input.txt");
-    private static final Path OUTPUT_FILE_PATH = Paths.get("output.txt");
-
-    @Rule
-    public ExternalResource inputFileResource = new ExternalResource() {
-        @Override
-        protected void before() throws Throwable {
-            Files.createFile(INPUT_FILE_PATH);
-        }
-
-        @Override
-        protected void after() {
-            try {
-                Files.deleteIfExists(INPUT_FILE_PATH);
-            } catch (IOException e) {
-                throw new RuntimeException("Could not delete file 'input.txt", e);
-            }
-        }
-    };
-
-    @Rule
-    public ExternalResource outputFileResource = new ExternalResource() {
-        @Override
-        protected void after() {
-            try {
-                Files.deleteIfExists(OUTPUT_FILE_PATH);
-            } catch (IOException e) {
-                throw new RuntimeException("Could not delete file 'output.txt'", e);
-            }
-        }
-    };
-
+public class SummatorTest extends FileBasedTest {
     @Test
-    public void testSimpleSum_1Plus2() throws Throwable {
+    public void testSimpleSum_1Plus2() {
         writeToInput("1 2");
 
         Summator.main(new String[0]);
 
-        assertThat("Invalid output file content", readFileToString(OUTPUT_FILE_PATH), is("3"));
+        assertOutputFileContent("3");
     }
 
     @Test
-    public void testSimpleSum_PositivePlusNegative() throws Throwable {
+    public void testSimpleSum_PositivePlusNegative() {
         writeToInput("1 -2");
 
         Summator.main(new String[0]);
 
-        assertThat("Invalid output file content", readFileToString(OUTPUT_FILE_PATH), is("-1"));
+        assertOutputFileContent("-1");
     }
 
     @Test
-    public void testSum_MaxPositiveValues() throws Throwable {
+    public void testSum_MaxPositiveValues() {
         writeToInput("1000000000 1000000000");
 
         Summator.main(new String[0]);
 
-        assertThat("Invalid output file content", readFileToString(OUTPUT_FILE_PATH), is("2000000000"));
+        assertOutputFileContent("2000000000");
     }
 
     @Test
-    public void testSum_MaxNegativeValues() throws Throwable {
+    public void testSum_MaxNegativeValues() {
         writeToInput("-1000000000 -1000000000");
 
         Summator.main(new String[0]);
 
-        assertThat("Invalid output file content", readFileToString(OUTPUT_FILE_PATH), is("-2000000000"));
-    }
-
-    private void writeToInput(String content) {
-        writeStringToFile(INPUT_FILE_PATH, content);
+        assertOutputFileContent("-2000000000");
     }
 }
